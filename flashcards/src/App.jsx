43dd9b3bc1d2262c6020a0151fd3ceb834dbdd13
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-26 20:27:35 +0100

    function Flashcards

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -36,8 +36,6 @@ const questions = [
 ];
 
 function App() {
-
-
   return (
     <>
       <FlashCards/>
@@ -47,6 +45,26 @@ function App() {
 
 
 function FlashCards() {
-  return <div>TODO</div>;
-}
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  )}
+
 export default App
